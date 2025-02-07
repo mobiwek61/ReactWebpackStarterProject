773ffe57287a6071af3021d8c187a785278b1d87
desktop14 <mobiwek61@gmail.com>
2025-02-07 17:22:17 -0500

    Update webpack.config_devServer.js
    added commments and cleaned up

--- webpack.config_devServer.js
+++ webpack.config_devServer.js
@@ -1,50 +1,34 @@
 
 /**  File webpack.config_devServer.js
- This is a javascript file which gets imported by webpack. It exports a big JSON object which
- directs webpack functionality. It's actually a configuration file.
- This file is setup for DEVELOPMENT, not for BUILD.
+ Run webpack with this command:
+ node node_modules/webpack/bin/webpack serve -c ./webpack.config_devServer.js 
+ What it does: pack all js files into a single minified .js file and run a development HTTP server.
 
- What it does: pack all js files into a single minified .js file and run a development HTTP server for
- development purposes.
+ Sequence of events:
+ - The webpack HTTP server serves up index.html which has <script src="/bundle_localdev.js" 
+   The script runs all javascript in the bundle not inside functions; this includes index.js which
+   starts up Reactjs and invokes React components in the bundle.
+   NOTE: when webpack is run in "serve" mode, the bundle.js file is NOT physically created; its virtual.
 
- Below command directs webpack to serve the application for development, with auto build when 
- source is modified: (for git-bash window). 
- COMMAND LINE >> node node_modules/webpack/bin/webpack serve -c ./webpack.config_devServer.js  
- 
- Below command creates a javascript bundle in a real file on filesystem. It uses a config file setup
- for a BUILD 
- COMMAND LINE (for other file not this one) >> node node_modules/webpack/bin/webpack build -c ./webpack.config_build.js  
- 
- ref: "https://webpack.js.org/configuration/module" used this to try do decipher whats going on here.
- Note about running webpack: 
- - Note that webpack is used as a command line utility and is NOT part of the bundle or the project.
- - If webpack is installed globally "npm install -g webpack" there is a simple
-   "webpack" command. I don't do it, instead I run node manually as above.
+ This is a javascript file which exports a big JSON object which webpack imports and uses to configure itself. 
+
+ ref: "https://webpack.js.org/configuration/module" 
+ Note that webpack is used as a command line utility and is NOT part of the bundle or the project.
+ If webpack is installed globally "npm install -g webpack" use the "webpack" command. 
+ I don't do it, instead I run node manually from node_modules as above. This avoid version conflicts.
 */
 
-const path = require('path');
+const path = require('path');  // npm package used below
 
-// to include my plugin. Not required I did it for fun.
+// To include my webpack plugin. It displays an ascii qr code in the window where webpack gets run, showing the url
+// of the website. Enables quick mobile viewing of the app. Not required I did it for fun.
 var MyWebpackPlugin_shows_URL_as_qrcode = require('./webpackUtils/MyWebpackPlugin_shows_URL_as_qrcode');
 
 module.exports = 
 {
-  // entry means "start dependency graph at this file", which follows casading import()'s
-  // The server serves up index.html which has <script src="/bundle_localdev.js" 
-  // Above script has effect of running all loose javascript commands not inside functions, in all files.
-  // NOTE: when webpack is run in "serve" mode, the bundle.js file is NOT physically created, instead it somehow
-  // makes the calling HTML file think it exists.
-  
-  // for production bundle, having only library functions
-  // NOT ME >>   entry: '../src/PackageTreeEntry.js',
-  
-  // for development bundle, include dev React startup code (has ReactDOM.createRoot(... ) plus the 
-  //    tree entry point.
-  entry: ['./src/index.js'],
-  // entry: ['./webpackServerDevApp/forDevBundle/startReactInDiv.js', '../src/PackageTreeEntry.js'],
+  // entry means "start dependency graph at this file", which follows casading import()'s (see the file)
+  entry: ['./src/index.js'],  // entry: ['./aaa.js', '../bbb.js'],
 
-  // below entry does nothing. It only serves as point to start dependency graph to build bundle
-  // entry: '../src/PackageTreeEntry.js',
   optimization: {
     // NOT ME >>    minimize: true // human readable if minimize if false
     minimize: false
@@ -57,89 +41,31 @@ module.exports =
   },
   // setup where to put minified output file "the bundle", and any assets like jpegs. 
   output: {
-    // NOT ME path: path.resolve(__dirname, '../../publicProj/npmjs_com/bundle-publish-public'), , // originally dist-webpack
+    path: path.resolve(__dirname, 'zzzzz'),  // seems to be ignored in dev mode, where physical file not created
 
-    // Resulting bundle file is NOT USED and can cause confusion.
-    // Here is the line to create the bundle file anyway. Note the "build" (not run) argument
-    // node ./node_modules/webpack/bin/webpack build -c ./webpack.config_devServer.js
-    // path: path.resolve(__dirname, 'webpackServerDevApp'), 
-    path: path.resolve(__dirname, 'zzzzz'), 
-
-    // NOT ME filename: 'bundle.js', // name of combined file, the "bundle"
-    // ME THIS FILENAME IS VIRTUAL BY THE DEV SERVER IT DOES NOT ACTUALLY EXIST
     filename: 'bundle_webpackServer.js',
-    // https://webpack.js.org/configuration/output/#outputlibrarytype
-    // library type 'window' will not create a bundle file. It's for running as a server, or clicking on index.html.
-    // I think it means "make library available via the DOM window object"
+    // ref: https://webpack.js.org/configuration/output/#outputlibrarytype
+    // type:'window' must mean "make library available via the DOM window object"
     library: { name: 'webPackLibraryVisibleFromDomWindowObject' , type: 'window' } 
-    // type: 'commonjs2' works on client but not local serve or clicking on index.html
-    // NOT ME >>  library: { type: 'commonjs2' } 
+    // type: 'commonjs2' works on client but not local server 
+    // ?? library: { type: 'commonjs2' } 
   },
 
   // plugin here is a way to run javascript code during build
-  // this shows url as a qrcode on console using IP address for mobile dev. Not required I did it for fun.
   plugins: [
-    // this one gets rid of annoying "download devtools" message..
-    //new (require('webpack')).DefinePlugin({'__REACT_DEVTOOLS_GLOBAL_HOOK__': '({ isDisabled: true })'}),
-    // my own plugin to show qr code ascii in console for mobile viewing
+    // see notes above
     new MyWebpackPlugin_shows_URL_as_qrcode({ options: true, urlpath: ':3003/sand' })],
 
-  // the 'npx create-react-app my-app' thing uses HtmlWebpackPlugin to insert 
-  // something like ' <script src="/bundle_webpackServer.js"></script>' into index.html before running so
-  // so the coder does not have to. I didn't know why the script tab was not needed and thought
-  // I was doing something wrong so searched how they did it and found this.
-  // Not using it because its a confusing useless thing to do; put script tag into index.html instead!
-  // Leave this comment here because it took hours to find how they did it.
-  // plugins: [
-  //   new HtmlWebpackPlugin({
-  //     //inject: true,
-  //     title: 'zzbb',
-  //     // template: 'bundle_webpackServer.js'
-  //     template: './webpackServerDevApp/index.js'
-  //   })
-  // ],
-  
-  // *********************************************************************************************************
-  // *** "EXTERNALS" FIXES ERROR ON CLIENT: WHEN REACT CONTROL IMPORTED FROM BUNDLE CALLS useEffect(), GET ***
-  // ***  ERROR  MESSAGE ABOUT DUPLICATE COPIES OF REACT. BELOW ENTRY SAYS DONT BUNDLE REACT, JUST USE     ***
-  // ***  CLIENT'S COPY OF REACT FOR THE GIVEN LIBRARY/TYPE ENTRIES.                                       ***
-  // *** [OK for npmjs publish but not local bundle or wedpack development server]                                                       ***
-  // *** https://webpack.js.org/configuration/externals/                                                   ***
-  // *** "The externals configuration option provides a way of excluding dependencies from the output      ***
-  // *** bundles. Instead, the created bundle relies on that dependency to be present in the consumer's    ***
-  // *** (any end-user application) environment. ..."                                                      ***
-  // *********************************************************************************************************
-  // // NOT ME >>    
-  // externals: {        
-  //   react: {          
-  //       commonjs: 'react',          
-  //       commonjs2: 'react',          
-  //       amd: 'React',          
-  //       root: 'React',      
-  //   },      
-  //   'react-dom': {          
-  //       commonjs: 'react-dom',          
-  //       commonjs2: 'react-dom',          
-  //       amd: 'ReactDOM',          
-  //       root: 'ReactDOM',      
-  //   },  
-  // },
-
-  // modules appears to be chunks of processing to do. 
-  // In this case, there's 1 module which calls babel to convert jsx in React source to plain js
+  // modules are chunks of processing to do. The "use" tag invokes npm packages to do various processing.
   module: {
     rules: [  // here's the first rule in the array of rules
-
-      ////////////////////    
-      { // feb 2025 added for sass, the css stylesheet thing
+      { // added for sass, the css stylesheet thing
         test: /\.(scss|sass)$/,
         // npm i sass-loader -D   ... installs and shows up in package.json as a dev-dependency (not part of project)        
         use: [ 'style-loader',  'css-loader', 'sass-loader' ] 
       },
-      //////////////////
-      
-      { 
-        test: /\.(js|jsx|ts|tsx)$/,   // feed files *.js and *.js to babel. ref:https://webpack.js.org/configuration/module/#ruletest
+      { // feeds files to babel, the JSX transpiler. It converts JSX to plain javascript. ref:https://webpack.js.org/configuration/module/#ruletest 
+        test: /\.(js|jsx|ts|tsx)$/,   
         // not needed... include: [  path.resolve(__dirname, "src/zzz"), path.resolve(__dirname, "src/abc") ],
         exclude: /node_modules/, // dont send these hundreds of files to babel! Client will download these itself upon "npm i"
         use: {  // ref: https://webpack.js.org/configuration/module/#ruleuse
@@ -152,21 +78,8 @@ module.exports =
               // for "optionalChainingAssign" error, but not needed. See comment in source ,"plugins": [ "@babel/plugin-proposal-optional-chaining" ]
             }
         }, 
-      }, // END of the babel rule
-
-      // { // START of the babel rule
-      //   test: /\.(js|jsx)$/,   // feed files *.js and *.js to babel. ref:https://webpack.js.org/configuration/module/#ruletest
-      //   // not needed... include: [  path.resolve(__dirname, "src/zzz"), path.resolve(__dirname, "src/abc") ],
-      //   exclude: /node_modules/, // dont send these hundreds of files to babel! Client will download these itself upon "npm i"
-      //   use: {  // ref: https://webpack.js.org/configuration/module/#ruleuse
-      //     loader: 'babel-loader', // node_modules/babel-loader/lib runs something here ...?
-      //     // NOTE: options can be left out but you need a .babelrc with same presets.
-      //     //       If you leave out both things, get fail without a description of whats wrong
-      //     options: { "presets": ["@babel/preset-env", "@babel/preset-react"] }
-      //   }, 
-      // }, // END of babel rule
-
-      {  // this rule includes .css files
+      }, 
+      {  // this rule eats .css files
         test: /\.css$/,
         use: [
             { loader: 'style-loader' },
@@ -176,9 +89,8 @@ module.exports =
 
     ]
   },
-  // devServer does not apply to build
-  // https://webpack.js.org/configuration/dev-server/#devserverclient
-  devServer: {
+  
+  devServer: {  // ref: https://webpack.js.org/configuration/dev-server/#devserverclient
     // https: true, // location on browser is denied if not https
     // allowedHosts: ['.host.com', 'host2.com'],
     // OK for only one static dir...    static: path.join(__dirname, './publicProj/npmjs_com/bundle-publish-public'), // originally dist-webpack
@@ -197,8 +109,7 @@ module.exports =
     host: 'local-ip', // makes open.target go to the IP, not localhost. Good if app has QR code.
     // https://webpack.js.org/configuration/dev-server/#devserveropen
     // open: false, //true, // Tells dev-server to open the browser
-    // below opens chrome to specific url
-    open: { target: ['/sand'], 
+    open: { target: ['/sand'], // opens chrome to specific url
             app: { name: 'msedge' // microsoft edge
             // app: { name: 'chrome'
             // infinite tabs!!   , arguments: ' --auto-open-devtools-for-tabs' 
@@ -214,3 +125,46 @@ module.exports =
     },
   }
 };
+
+// THE EXTERNALS SECTION, NOT NEEDED FOR DEVELOPMENT BUILD
+// the 'npx create-react-app my-app' thing uses HtmlWebpackPlugin to insert 
+  // something like ' <script src="/bundle_webpackServer.js"></script>' into index.html before running so
+  // so the coder does not have to. I didn't know why the script tab was not needed and thought
+  // I was doing something wrong so searched how they did it and found this.
+  // Because this confused me, I put script tag into index.html instead!
+  // Leave this comment here because it took hours to find how they did it.
+  // plugins: [ new HtmlWebpackPlugin({
+  //     //inject: true,   title: 'zzbb', // template: 'bundle_webpackServer.js'
+  //     template: './webpackServerDevApp/index.js' })],
+  
+  // *********************************************************************************************************
+  // *** "EXTERNALS" FIXES ERROR ON CLIENT: WHEN REACT CONTROL IMPORTED FROM BUNDLE CALLS useEffect(), GET ***
+  // ***  ERROR  MESSAGE ABOUT DUPLICATE COPIES OF REACT. BELOW ENTRY SAYS DONT BUNDLE REACT, JUST USE     ***
+  // ***  CLIENT'S COPY OF REACT FOR THE GIVEN LIBRARY/TYPE ENTRIES.                                       ***
+  // *** [OK for npmjs publish but not local bundle or wedpack development server]                                                       ***
+  // *** https://webpack.js.org/configuration/externals/                                                   ***
+  // *** "The externals configuration option provides a way of excluding dependencies from the output      ***
+  // *** bundles. Instead, the created bundle relies on that dependency to be present in the consumer's    ***
+  // *** (any end-user application) environment. ..."                                                      ***
+  // *********************************************************************************************************
+  /* 
+    this is needed for publishing a bundle to npmjs, but not for
+    deploying a whole react app to nginx or github pages.
+    That's because it needs react.js of its own; there is no react app importing t
+    the bundle; an app which already has its own react. 
+  */
+  // externals: {        
+  //   react: {          
+  //       commonjs: 'react',          
+  //       commonjs2: 'react',          
+  //       amd: 'React',          
+  //       root: 'React',      
+  //   },      
+  //   'react-dom': {          
+  //       commonjs: 'react-dom',          
+  //       commonjs2: 'react-dom',          
+  //       amd: 'ReactDOM',          
+  //       root: 'ReactDOM',      
+  //   },  
+  // },
+
